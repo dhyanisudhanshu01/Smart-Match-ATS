dfee5e2020d3dfda4e041a96187f4c5bd7fc4eef
dhyanisudhanshu01 <dhyanisudhanshu01@gmail.com>
2026-02-06 19:49:58 +0530

    Project is completed and ready for deployement

--- src/app.py
+++ src/app.py
@@ -1,5 +1,30 @@
 import streamlit as st
+from engine import extract_text_from_pdf, compute_similarity, gemini_gap_analysis
 
-st.title("Smart-Match ATS")
+st.set_page_config(
+    page_title="Smart-Match ATS",
+    page_icon=":briefcase:",
+    layout="centered",
+)
 
-st.write("Welcome to the Smart-Match Applicant Tracking System (ATS)!")
+st.title("💼 - Smart-Match ATS")
+st.subheader("AI Resume Matcher")
+
+uploaded_resume = st.file_uploader("Upload your resume (PDF format)", type=["pdf"])
+
+job_description = st.text_area("Paste Job Description")
+
+if st.button("Analyze Resume"):
+    if uploaded_resume and job_description:
+        with st.spinner("Analyzing..."):
+            resume_text = extract_text_from_pdf(uploaded_resume)
+            similarity_score = compute_similarity(resume_text, job_description)
+            feedback = gemini_gap_analysis(resume_text, job_description)
+
+        st.metric("Match Score:", round(similarity_score, 2))
+        st.subheader("Feedback:")
+        st.markdown(feedback)
+
+        
+    else:
+        st.error("Please upload a resume and paste a job description to analyze.")
--- src/engine.py
+++ src/engine.py
@@ -1,10 +1,37 @@
 import pdfplumber
+from sentence_transformers import SentenceTransformer,util
+from dotenv import load_dotenv
+import os
+import re
+import google.generativeai as genai   
 
-def extract_text_from_pdf(pdf_path):
+# Load the pre-trained SentenceTransformer model
+load_dotenv()  # Load environment variables from .env file
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+gemini_model = genai.GenerativeModel("gemini-2.5-flash")
+embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def normalize_text(text):
+
+    text = text.lower()
+
+    text = text.replace("\n", " ").replace("\t", " ")
+
+    text = re.sub(r"[^a-z0-9\s]", " ", text)
+
+    text = re.sub(r"\s+", " ", text).strip()
+
+    words = text.split()
+    words = [w for w in words if len(w) > 2]
+
+    return " ".join(words)
+
+def extract_text_from_pdf(file):
     #extract text from a PDF file
 
     text = ""
-    with pdfplumber.open(pdf_path) as pdf:
+    with pdfplumber.open(file) as pdf:
         for page in pdf.pages:
             page_text = page.extract_text()
             if page_text:
@@ -12,7 +39,49 @@ def extract_text_from_pdf(pdf_path):
 
     return text.strip()
 
+
+def compute_similarity(resumetext, jobdescription):
+    # Compute the cosine similarity between two texts
+
+    # Encode the texts into embeddings
+    jobdescription = normalize_text(jobdescription)
+
+    resumetext = normalize_text(resumetext)
+
+    embeddings = embedding_model.encode([resumetext, jobdescription], convert_to_tensor=True)
+
+    # Compute cosine similarity
+    similarity = util.cos_sim(embeddings[0], embeddings[1])[0][0]
+
+    return float(similarity * 100)
+
+def gemini_gap_analysis(resume_text, job_description):
+    # Use Gemini to analyze the gap between resume and job description
+
+    prompt = f"""
+    You are an ATS system.
+
+    Compare the following Resume and Job Description.
+
+    Tasks:
+    1. List missing skills.
+    2. Suggest resume improvements.
+    3. Give short professional feedback.
+
+    Resume:
+    {resume_text}
+
+    Job Description:
+    {job_description}
+    """
+
+    response = gemini_model.generate_content(prompt)
+    return response.text
+
 if __name__ == "__main__":
     pdf_path = "data/sample_resume.pdf"  # Replace with your PDF file path
     extracted_text = extract_text_from_pdf(pdf_path)
-    print(extracted_text)
+    jd_text = "We are looking for a motivated Accounting & Audit Fresher to support day-to-day accounting activities and assist in audit assignments. The candidate will gain hands-on exposure to bookkeeping, financial records, and compliance processes."
+    print("Matching Score:", compute_similarity(extracted_text, jd_text))
+    print("\nGemini Feedback:\n")
+    print(gemini_gap_analysis(extracted_text, jd_text))
